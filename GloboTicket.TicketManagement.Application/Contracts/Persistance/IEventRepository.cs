49b1b4222f4c5679d83bd9c0fe71d108a1315a68
Runtime-Ali <57050083+Runtime-Ali@users.noreply.github.com>
2021-01-11 17:25:07 -0500

    Module 1-4 completed

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistance/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistance
+{
+    public interface IEventRepository : IAsyncRepository<Event>
+    {
+        Task<bool> IsEventNameAndDateUnique(string name, DateTime date);
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Exceptions
+{
+    class ValidationException : ApplicationException
+    {
+        public List<string> ValidationErrors { get; set; }
+
+        public ValidationException(ValidationResult validationResult)
+        {
+            ValidationErrors = new List<string>();
+            foreach (var validationError in validationResult.Errors)
+            {
+                ValidationErrors.Add(validationError.ErrorMessage);
+            }
+        }
+    }
+}
